b98300fc9af40d3775ed8bdc2d316d6084f27b2f
Yuriy Shukan <120783152+Yuriy2223@users.noreply.github.com>
2026-03-21 11:55:41 +0200

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,4 @@
-import { LogoDecorative } from "@/components/Logo/LogoDecorative";
+import { LogoDecorative } from '@/components/Logo/LogoDecorative';
 
 export default function Loading() {
   return (
