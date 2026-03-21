224bfb1a65abbf23015f2a8bd7155dbc38f491b4
Yuriy Shukan <120783152+Yuriy2223@users.noreply.github.com>
2026-03-21 11:53:44 +0200

    Update page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,18 +1,17 @@
-import { Hero } from "@/components/Hero/Hero";
-import { Atmosphere } from "@/components/Atmosphere/Atmosphere";
-import { OurMenu } from "@/components/OurMenu/OurMenu";
-import { About } from "@/components/About/About";
-import { Gallery } from "@/components/Gallery/Gallery";
-import { Reviews } from "@/components/Reviews/Reviews";
-import { CoffeeQuiz } from "@/components/CoffeeQuiz/CoffeeQuiz";
-import { CoffeeOrigins } from "@/components/CoffeeOrigins/CoffeeOrigins";
-import { Events } from "@/components/Events/Events";
-import { ContactForm } from "@/components/ContactForm/ContactForm";
-import { ToastContainer } from "react-toastify";
+import { Hero } from '@/components/Hero/Hero';
+import { Atmosphere } from '@/components/Atmosphere/Atmosphere';
+import { OurMenu } from '@/components/OurMenu/OurMenu';
+import { About } from '@/components/About/About';
+import { Gallery } from '@/components/Gallery/Gallery';
+import { Reviews } from '@/components/Reviews/Reviews';
+import { CoffeeQuiz } from '@/components/CoffeeQuiz/CoffeeQuiz';
+import { CoffeeOrigins } from '@/components/CoffeeOrigins/CoffeeOrigins';
+import { Events } from '@/components/Events/Events';
+import { ContactForm } from '@/components/ContactForm/ContactForm';
+import { ToastContainer } from 'react-toastify';
 
 export default async function Home() {
-
-  await new Promise((resolve) => setTimeout(resolve, 500));
+  await new Promise(resolve => setTimeout(resolve, 1000));
 
   return (
     <main>
