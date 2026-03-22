da7a2b5e2c5e72932b1aafce6c41b8022e9adac8
Yuriy Shukan <120783152+Yuriy2223@users.noreply.github.com>
2026-03-22 20:08:43 +0200

    Update page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -15,7 +15,7 @@ async function getHeroData() {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/api/hero`,
       {
-        cache: 'no-store',
+        next: { revalidate: 60 },
       }
     );
     if (!response.ok) throw new Error('Failed to fetch hero data');
